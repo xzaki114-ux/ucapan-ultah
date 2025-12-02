f99597e8d2b3453831c95255a8ac93d9b3447eea
xzaki114-ux <xzaki114@gmail.com>
2025-12-02 21:14:42 +0800

    Update welcome message for successful login

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ function validateForm() {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang ayanggg",
+      text: "Selamat datang Zaki",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
