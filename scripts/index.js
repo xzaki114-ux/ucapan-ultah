592454347f309303dea6d7b8972cdd343ddfbe84
xzaki114-ux <xzaki114@gmail.com>
2025-12-03 10:52:23 +0800

    Change validation credentials to 'Zaki'
    Updated validation logic to check for username and password 'Zaki'.

--- scripts/index.js
+++ scripts/index.js
@@ -6,11 +6,11 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 });
 
 function validateForm() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  var username = document.getElementById("Zaki").value;
+  var password = document.getElementById("Zaki").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "user" && password === "user") {
+  if (username === "Zaki" && password === "Zaki") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
